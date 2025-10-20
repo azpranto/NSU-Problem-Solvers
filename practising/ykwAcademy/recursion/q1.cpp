072066245801b7986de28ec53d107b7e2a38d288
azpranto <azpranto11@gmail.com>
2025-10-21 00:32:13 +0600

    easy

---
+++ practising/ykwAcademy/recursion/q1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void rec(int n) {
+    if (n == 0) {
+        return;
+    } else {
+        cout << "I love Recursion" << '\n';
+        return rec(n - 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    rec(n);
+
+    return 0;
+}
